117cc262df7eaa8ca15a854c6599bb01cd825f08
Alex Jack <alex@Alexs-MacBook-Air.local>
2023-05-22 15:18:23 +0100

    first commit

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, cohort, fav_language):
+        self.name = name
+        self.cohort = cohort
+        self.fav_language = fav_language
+
+    def talk():
+        return "I can talk!"
+
+    def say_favourite_language(self, language):
+        return (f'My favourite langauge is' language)
